108839027c7fb84a8ea55a2a24ebe28cc66d6cd1
omg <omg@orionsbelt.space>
2018-05-14 15:33:45 -0800

    ?

--- rclone_copy.sh
+++ rclone_copy.sh
@@ -2,7 +2,7 @@
 
 # Usage: create a bash script that will call this script from github.
 # Example: curl -s https://raw.githubusercontent.com/The-OMG/rclone_tools/master/rclone_copy.sh | bash /dev/stdin httpremote: mygsuite:
-
+#
 REMOTE="$1"
 GDRIVE_REMOTE="$2"
 LOGFILE="${HOME}/logs/$(date +"%d-%m-%Y_%H%M%S")-rclone_copy.log"
